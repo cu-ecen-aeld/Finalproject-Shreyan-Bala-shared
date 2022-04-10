84176285018017e8223a5e9dfae118ccc205dc58
bala9248 <balapranesh99@gmail.com>
2022-04-09 22:28:02 -0600

    Test

--- client_package/client.c
+++ client_package/client.c
@@ -10,6 +10,70 @@
 #include <sys/socket.h>
 #include <syslog.h>
 
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+void func(int sockfd)
+{
+	char buff[MAX];
+	int n;
+	for (;;) {
+		bzero(buff, sizeof(buff));
+		read(sockfd, buff, sizeof(buff));
+		printf("\nFrom Server : %s\n", buff);
+	}
+}
+
+int main (int argc, char *argv[])
+{
+	int sockfd, connfd;
+	struct sockaddr_in servaddr, cli;
+
+	// socket create and verification
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		printf("socket creation failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully created..\n");
+	bzero(&servaddr, sizeof(servaddr));
+	char server_ipaddress[20] = {0};
+	memcpy(server_ipaddress, argv[1], strlen(argv[1]));
+	
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(server_ipaddress);
+	servaddr.sin_port = htons(PORT);
+
+	// connect the client socket to server socket
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+		printf("connection with the server failed...\n");
+		exit(0);
+	}
+	else
+		printf("connected to the server..\n");
+
+	// function for chat
+	func(sockfd);
+
+	// close the socket
+	close(sockfd);
+}
+
+
+/*#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <syslog.h>
+
 #define IP_ADDRESS_SIZE 20
 
 #define PORT 8080
@@ -69,4 +133,4 @@ int main (int argc, char *argv[])
 
 
 }
-
+*/
