30439458eb2930fd3bc2f5df1f1038b335db911a
bala9248 <balapranesh99@gmail.com>
2022-04-14 22:21:49 -0600

    Modified OLED code

--- oled_package/oled.cpp
+++ oled_package/oled.cpp
@@ -11,8 +11,15 @@ int main( int argc, char * argv [] ) {
 	myDisplay.initDisplay();
 	myDisplay.clearDisplay();
 	myDisplay.setWordWrap(TRUE);
-	myDisplay.setDisplayMode(SSD1306::Mode::WRAP);
+	myDisplay.setDisplayMode(SSD1306::Mode::SCROLL);
+	myDisplay.textDisplay("THIS IS THE FIRST LINE.");
+	myDisplay.textDisplay("LONG WRAP THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG LONG WRAP THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG LONG WRAP THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG");
+	myDisplay.textDisplay("PUNCTUATION !\"#$%&'()*+,-./:;<=>?@[\\]^_`");
 	myDisplay.textDisplay("NUMBERS 0123456789");
+	myDisplay.setWordWrap(FALSE);
+	myDisplay.textDisplay("LONG NO WRAP THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG");
+	myDisplay.textDisplay("THIS IS THE LAST LINE.");
+	myDisplay.textDisplay("DISPLAY WRAP.");
 
 /*
 	SSD1306 myDisplay;
