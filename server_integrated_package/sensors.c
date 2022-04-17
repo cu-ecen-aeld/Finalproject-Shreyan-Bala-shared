9179524ba2fedd6279b11918b2de90a53f99bc37
bala9248 <balapranesh99@gmail.com>
2022-04-17 17:18:05 -0600

    Modified roll value

--- server_integrated_package/sensors.c
+++ server_integrated_package/sensors.c
@@ -210,7 +210,7 @@ int main(int argc, char **argv) {
     	bme280();
     	mpu6050();
 
-    	int roll = atan2(yaccel, zaccel)* 180 / 3.14159265;
+    	int roll = (atan2(yaccel, zaccel)* 180 / 3.14159265) + 100;
     	
     	snprintf(message.mesg_text, sizeof(message.mesg_text), "roll%d Temp%d Tyre%d", (int)roll, (int)temp, (int)station_press);
     	
